1870e3f4aa525ca10dbe6df3a9178aaf5e732cb7
Pratik <sahap187@gmail.com>
2021-12-31 18:03:52 +0530

    word counter added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 
-import './App.css';
 import TextForm from './component/TextForm';
 import Textutil from './component/Textutil';
 
 function App() {
   return (
-    <div className="App">
+    <div >
      <Textutil/>
      <div className="container my-3">
        <TextForm heading='Enter your text to Analyze'/>
--- src/component/TextForm.jsx
+++ src/component/TextForm.jsx
@@ -7,18 +7,41 @@ export default function TextForm({heading}){
        let newtext=text.toUpperCase()
        setText(newtext)
     }
+    
+    const handelloclick=()=>{
+        // console.log("btn clicked")
+        let newtext=text.toLowerCase()
+        setText(newtext)
+     }
+
     const handelChange=(e)=>{
        // console.log(e.target.value)
        setText(e.target.value)
     }
 return(
-    <div>
+    <>
+    <div className="container">
        <h1>{heading}</h1>
         <div className="mb-3">
   <textarea className="form-control" value={text} onChange={handelChange} id="mytext" rows="8"></textarea>
         </div>
-   <button type="button" onClick={handelUpclick} className="btn btn-primary">convert to Uppercase</button>
+   <button type="button" onClick={handelUpclick} className="btn btn-primary mx-2">convert to Uppercase</button>
+   <button type="button" onClick={handelloclick} className="btn btn-primary mx-2">convert to Lowercase</button>
     </div>
+
+     <div className="container my-3">
+         <h1>Your text Summary</h1>
+         <p>{text.split(" ").length} words and {text.length} chaacters</p>
+         <p>{0.8 * text.split(" ").length} Minutes read</p>
+          
+          <h1>Preview</h1>
+          <div className="container my-2">
+            {text}
+          </div>
+
+     </div>
+
+    </>
 )
 
 }
